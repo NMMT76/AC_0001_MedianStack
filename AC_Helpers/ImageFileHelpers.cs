4cc6799e6c4909017e2d4d7e8d58a2a81ac89ada
NT <NT@DESKTOP-TGBSDKO>
2023-05-09 00:17:25 +0100

    Add project files.

---
+++ AC_Helpers/ImageFileHelpers.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace AC_0001_MedianStack.AC_Helpers
+{
+	public static class ImageFileHelpers
+	{
+		public static byte[,,] LoadImageToArray(string sourceimage)
+		{
+			byte[,,] retval;
+			using (Image<Rgb24> image = Image.Load<Rgb24>(sourceimage))
+			{
+				int width = image.Width;
+				int height = image.Height;
+				retval = new byte[width,height,3];
+				for (int y = 0; y < height - 1; y++)
+				{
+					for (int x = 0; x < width - 1; x++)
+					{
+						Rgb24 pixel = image[x, y];
+						retval[x, y, 0] = pixel.R;
+						retval[x, y, 1] = pixel.G;
+						retval[x, y, 2] = pixel.B;
+					}
+				}
+			}
+			return retval;
+		}
+		public static void SaveArrayToImage(byte[,,] imagebuffer,string destination)
+		{
+			int width = imagebuffer.GetLength(0);
+			int height = imagebuffer.GetLength(1);
+			using (Image<Rgb24> image = new Image<Rgb24>(width,height))
+			{
+				for (int y = 0; y < height - 1; y++)
+				{
+					for (int x = 0; x < width - 1; x++)
+					{
+						image[x, y] =new Rgb24(imagebuffer[x, y, 0], imagebuffer[x, y, 1], imagebuffer[x, y, 2]);
+					}
+				}
+				image.SaveAsPng(destination);
+			}
+		}
+		public static byte[,,] CalculateMedianImage(List<byte[,,]> imagebuffers,int runlength)
+		{
+			byte[,,] retval;
+			int width = imagebuffers[0].GetLength(0);
+			int height = imagebuffers[0].GetLength(1);
+			retval = new byte[width, height, 3];
+			List<byte> compr = new List<byte>();
+			List<byte> compg = new List<byte>();
+			List<byte> compb = new List<byte>();
+			//
+			for (int y = 0; y < height - 1; y++)
+			{
+				for (int x = 0; x < width - 1; x++)
+				{
+					for (int rl = 0; rl < runlength - 1; rl++)
+					{
+						compr.Add(imagebuffers[rl][x, y, 0]);
+						compg.Add(imagebuffers[rl][x, y, 1]);
+						compb.Add(imagebuffers[rl][x, y, 2]);
+					}
+					//Sort the lists and extract the value at the middle
+					compr.Sort();
+					retval[x, y, 0] = compr[(int)(compr.Count/2.0)];
+					compg.Sort();
+					retval[x, y, 1] = compg[(int)(compg.Count / 2.0)];
+					compb.Sort();
+					retval[x, y, 2] = compb[(int)(compb.Count / 2.0)];
+					//
+					compr.Clear();compg.Clear();compb.Clear();
+				}
+			}
+			return retval;
+		}
+		public static Tuple<double,int,int> CalculateDiff(byte[,,] buffer1, byte[,,] buffer2)
+		{
+			int width = buffer1.GetLength(0);
+			int height = buffer1.GetLength(1);
+			double rmserror=0;
+			int mindiff = 3*255, maxdiff = 0;
+			//
+			for (int y = 0; y < height - 1; y++)
+			{
+				for (int x = 0; x < width - 1; x++)
+				{
+					for (int c = 0; c < 3; c++)
+					{
+						int absdiff = Math.Abs((int)buffer1[x, y, c] - (int)buffer2[x, y, c]);
+						rmserror += Math.Pow(absdiff, 2);
+						if ((absdiff>0)&&(absdiff < mindiff)) { mindiff = absdiff; }
+						if (absdiff > maxdiff) { maxdiff = absdiff; }
+					}
+				}
+				rmserror = Math.Sqrt(rmserror / (height * width * 3));
+			}
+			return new Tuple<double,int,int>(rmserror, mindiff, maxdiff);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using AC_0001_MedianStack.AC_Helpers;
+
+namespace AC_0001_MedianStack
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Source directory for the images
+			string baseimagesourcedir = System.Environment.CurrentDirectory+@"\MedianStackImages\";
+			string outdir = System.Environment.CurrentDirectory;
+
+			List<byte[,,]> imagebuffers = new List<byte[,,]>();
+			
+			//Load images into the list of imagebuffers
+			for (int counter = 0; counter < 10; counter++)
+			{
+				string imagefile = (counter+1).ToString().PadLeft(4,'0');
+				byte[,,] imagebuffer = ImageFileHelpers.LoadImageToArray($"{baseimagesourcedir}\\{imagefile}.png");
+				imagebuffers.Add(imagebuffer);
+			}
+
+			ImageFileHelpers.SaveArrayToImage(imagebuffers[0], outdir + "\\median00.png");
+
+
+			//Declare the output buffers
+			byte[,,] buffermed5,buffermed10;
+
+			//Calculate the median buffers
+			buffermed5 = ImageFileHelpers.CalculateMedianImage(imagebuffers, 5);
+			buffermed10 = ImageFileHelpers.CalculateMedianImage(imagebuffers, 10);
+
+			OutputDiff(ImageFileHelpers.CalculateDiff(imagebuffers[0], buffermed10), nameof(imagebuffers), nameof(buffermed10));
+			OutputDiff(ImageFileHelpers.CalculateDiff(buffermed5, buffermed10), nameof(buffermed5), nameof(buffermed10));
+
+			//Output the median images
+			ImageFileHelpers.SaveArrayToImage(buffermed5, outdir + "\\median05.png");
+			ImageFileHelpers.SaveArrayToImage(buffermed10, outdir + "\\median10.png");
+		}
+		public static void OutputDiff(Tuple<double,int,int> diffs,string name1,string name2)
+		{
+			Console.WriteLine($"Diff between {name1} and {name2}");
+			Console.WriteLine($"RMS : {diffs.Item1} - Min : {diffs.Item2} - Max : {diffs.Item3}");
+		}
+	}
+}
